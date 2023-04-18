e062a1f119e96bebc4fdc2e207496d89a80f29c9
mishany777 <misha_ekb777@mail.ru>
2023-04-18 15:25:26 +0500

    new design

---
+++ app/src/main/java/com/example/stoplyingdown/new_sex.java
@@ -0,0 +1,14 @@
+package com.example.stoplyingdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_sex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_sex);
+    }
+}

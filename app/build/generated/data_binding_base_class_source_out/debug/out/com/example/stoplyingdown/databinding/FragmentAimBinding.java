7f0d2b363d51fb4b99c11e1f2170b715deccb1cc
mishany777 <misha_ekb777@mail.ru>
2023-04-26 21:12:21 +0500

    new profile

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/stoplyingdown/databinding/FragmentAimBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/stoplyingdown/databinding/FragmentAimBinding.java
@@ -4,25 +4,103 @@ package com.example.stoplyingdown.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.stoplyingdown.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentAimBinding implements ViewBinding {
   @NonNull
-  private final FrameLayout rootView;
+  private final ConstraintLayout rootView;
 
-  private FragmentAimBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final SwitchCompat switch10;
+
+  @NonNull
+  public final SwitchCompat switch12;
+
+  @NonNull
+  public final SwitchCompat switch2;
+
+  @NonNull
+  public final SwitchCompat switch6;
+
+  @NonNull
+  public final SwitchCompat switch8;
+
+  @NonNull
+  public final SwitchCompat switch9;
+
+  @NonNull
+  public final TextView textView15;
+
+  @NonNull
+  public final TextView textView16;
+
+  @NonNull
+  public final TextView textView17;
+
+  @NonNull
+  public final TextView textView18;
+
+  @NonNull
+  public final TextView textView22;
+
+  @NonNull
+  public final TextView textView25;
+
+  @NonNull
+  public final TextView textView26;
+
+  @NonNull
+  public final TextView textView28;
+
+  @NonNull
+  public final TextView textView29;
+
+  @NonNull
+  public final TextView textView30;
+
+  @NonNull
+  public final TextView textView31;
+
+  private FragmentAimBinding(@NonNull ConstraintLayout rootView, @NonNull SwitchCompat switch10,
+      @NonNull SwitchCompat switch12, @NonNull SwitchCompat switch2, @NonNull SwitchCompat switch6,
+      @NonNull SwitchCompat switch8, @NonNull SwitchCompat switch9, @NonNull TextView textView15,
+      @NonNull TextView textView16, @NonNull TextView textView17, @NonNull TextView textView18,
+      @NonNull TextView textView22, @NonNull TextView textView25, @NonNull TextView textView26,
+      @NonNull TextView textView28, @NonNull TextView textView29, @NonNull TextView textView30,
+      @NonNull TextView textView31) {
     this.rootView = rootView;
+    this.switch10 = switch10;
+    this.switch12 = switch12;
+    this.switch2 = switch2;
+    this.switch6 = switch6;
+    this.switch8 = switch8;
+    this.switch9 = switch9;
+    this.textView15 = textView15;
+    this.textView16 = textView16;
+    this.textView17 = textView17;
+    this.textView18 = textView18;
+    this.textView22 = textView22;
+    this.textView25 = textView25;
+    this.textView26 = textView26;
+    this.textView28 = textView28;
+    this.textView29 = textView29;
+    this.textView30 = textView30;
+    this.textView31 = textView31;
   }
 
   @Override
   @NonNull
-  public FrameLayout getRoot() {
+  public ConstraintLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +121,117 @@ public final class FragmentAimBinding implements ViewBinding {
 
   @NonNull
   public static FragmentAimBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.switch10;
+      SwitchCompat switch10 = ViewBindings.findChildViewById(rootView, id);
+      if (switch10 == null) {
+        break missingId;
+      }
 
-    return new FragmentAimBinding((FrameLayout) rootView);
+      id = R.id.switch12;
+      SwitchCompat switch12 = ViewBindings.findChildViewById(rootView, id);
+      if (switch12 == null) {
+        break missingId;
+      }
+
+      id = R.id.switch2;
+      SwitchCompat switch2 = ViewBindings.findChildViewById(rootView, id);
+      if (switch2 == null) {
+        break missingId;
+      }
+
+      id = R.id.switch6;
+      SwitchCompat switch6 = ViewBindings.findChildViewById(rootView, id);
+      if (switch6 == null) {
+        break missingId;
+      }
+
+      id = R.id.switch8;
+      SwitchCompat switch8 = ViewBindings.findChildViewById(rootView, id);
+      if (switch8 == null) {
+        break missingId;
+      }
+
+      id = R.id.switch9;
+      SwitchCompat switch9 = ViewBindings.findChildViewById(rootView, id);
+      if (switch9 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView15;
+      TextView textView15 = ViewBindings.findChildViewById(rootView, id);
+      if (textView15 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView16;
+      TextView textView16 = ViewBindings.findChildViewById(rootView, id);
+      if (textView16 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView17;
+      TextView textView17 = ViewBindings.findChildViewById(rootView, id);
+      if (textView17 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView18;
+      TextView textView18 = ViewBindings.findChildViewById(rootView, id);
+      if (textView18 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView22;
+      TextView textView22 = ViewBindings.findChildViewById(rootView, id);
+      if (textView22 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView25;
+      TextView textView25 = ViewBindings.findChildViewById(rootView, id);
+      if (textView25 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView26;
+      TextView textView26 = ViewBindings.findChildViewById(rootView, id);
+      if (textView26 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView28;
+      TextView textView28 = ViewBindings.findChildViewById(rootView, id);
+      if (textView28 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView29;
+      TextView textView29 = ViewBindings.findChildViewById(rootView, id);
+      if (textView29 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView30;
+      TextView textView30 = ViewBindings.findChildViewById(rootView, id);
+      if (textView30 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView31;
+      TextView textView31 = ViewBindings.findChildViewById(rootView, id);
+      if (textView31 == null) {
+        break missingId;
+      }
+
+      return new FragmentAimBinding((ConstraintLayout) rootView, switch10, switch12, switch2,
+          switch6, switch8, switch9, textView15, textView16, textView17, textView18, textView22,
+          textView25, textView26, textView28, textView29, textView30, textView31);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
